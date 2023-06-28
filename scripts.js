0e5c6656fabe9fb9fa21d73cf8426e26532d8d0d
Bappaditya sing <104288057+Bappaditya721135@users.noreply.github.com>
2023-06-28 09:11:00 +0530

    complet lazy loading functionality

---
+++ scripts.js
@@ -0,0 +1,34 @@
+const allImgs = document.querySelectorAll(".img")
+
+
+// observer callback 
+const observerCallback = (entries, observer) => {
+    const [entry] = entries;
+    const targetElment = entry.target
+
+    if(!entry.isIntersecting) return;
+
+
+    const newImgSrc = targetElment.dataset.src;
+
+    // changing the src of img 
+    targetElment.src = newImgSrc;
+
+    // remove the blur class after image doen loading 
+    targetElment.addEventListener("load", () => {
+        targetElment.classList.remove("blur")
+    })
+
+    // stop observing after loading 
+    observer.unobserve(targetElment)
+}
+
+// observer api 
+const observer = new IntersectionObserver(observerCallback, {root: null,rootMargin: "100px", threshold: "0",})
+
+allImgs.forEach(img => {
+    img.classList.add("blur")
+    observer.observe(img)
+})
+
+// document.addEventListener
